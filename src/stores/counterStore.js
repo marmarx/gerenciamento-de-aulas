b24eaa7627b7d570c6b9c63a6d4cac1082521c7c
marmarx <78326372+marmarx@users.noreply.github.com>
2025-12-10 16:32:23 -0300

    Delete src directory

--- src/stores/counterStore.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia"
-import { ref } from "vue"
-
-export const useClockStore = defineStore("clock", () => {
-  const now = ref(Date.now())
-  setInterval(() => { now.value = Date.now() }, 500) // ticks every 1 second -> miliseconds
-
-  return { now }
-})
