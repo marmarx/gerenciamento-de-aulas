9b3628e58d7a5d8c28748d91db0e69463d5481e3
marmarx <78326372+marmarx@users.noreply.github.com>
2025-12-11 18:11:26 -0300

    Add files via upload

--- src/logger.js
+++ src/logger.js
@@ -1,25 +1,70 @@
-// customizing console.log
-const DEBUG = true
-const consoleColors = {dataStore: 'red', Notification: 'blue'}
+// usage: import '@/logger.js'
+const DEBUG = true  // import.meta.env.DEV || false
 const originalLog = console.log
 
+// reset console log to support tag coloring
 console.log = (...args) => {
-  if(!DEBUG) return
+  if (!DEBUG) return
 
   const match = args
     .map(arg => typeof arg === 'string'
-      ? arg.match(/\[([^\]]*)\] ?/)
+      ? arg.match(/\[([^\]]*)\] ?/) // matchs '[any string]' or '[any string] '
       : null).find(m => m)
-      
+
   if (!match) return originalLog(...args)
 
-  const tag = match[1]
+  // const assignedColors = {}  // use this when specific colors for specific tags is desired
 
-  args = args
-    .map(arg => typeof arg === 'string'
-      ? arg.replace(/\[[^\]]*\] ?/g, "")
-      : arg
+  const tag = match[1]
+  const color = setColorTag(tag)  // assignedColors[tag]
+
+  const head = `%c[${tag}]`
+  const style = `color: ${color}; font-weight:bold`
+
+  args = args.map(arg => typeof arg === 'string'
+    ? arg.replace(/\[[^\]]*\] ?/g, "")
+    : arg
   )
 
-  return originalLog(`%c[${tag}]`, `color: ${consoleColors[tag]}; font-weight:bold`, ...args)
+  return originalLog(head, style, ...args)
 }
+
+// assigns a color to each tag that shows up in the console.log
+const createColorTag = () => {
+  let colorIndex = 0
+  const tagColors = new Map()   // tag -> assigned color
+  
+  const COLOR_PALETTE = [
+    '#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
+    '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+    '#008080', '#e6beff', '#9A6324', '#fffac8', '#800000',
+    '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080'
+  ]
+
+  const findColorTag = (tag) => {
+    if (!tagColors.has(tag)) {
+      const color = COLOR_PALETTE[colorIndex % COLOR_PALETTE.length]
+      tagColors.set(tag, color)
+      colorIndex++
+    }
+    return tagColors.get(tag)
+  }
+
+  return findColorTag
+}
+
+const setColorTag = createColorTag()
+
+/*
+// -- Defining setColorTag() anonymously --
+// -> named is better when it comes to legibility, reusability and tracing
+// -> makes no difference in terms of performance and tree shaking
+// -> for the sake of learning only
+
+const setColorTag = (() => {  // anonymous function
+  let colorIndex = 0
+  ...
+  return tag => {...}         // return anonympus function
+})()                          // run anonymous function
+
+*/
