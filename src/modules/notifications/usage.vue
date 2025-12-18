b3f6edcdefe6c47dde2082fc6fd08d11f1162b29
marmarx <78326372+marmarx@users.noreply.github.com>
2025-12-18 15:29:42 -0300

    Delete src directory

--- src/modules/notifications/usage.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { useNotificationStore } from '@/modules/notifications/notificationStore'
-useNotificationStore()
-
-// testing
-import { testNotification } from '@/modules/notifications/notificationMain'
-await testNotification()
-</script>
--- src/modules/toast/toastShow.js
+++
@@ -1,6 +0,0 @@
-import { useToastStore } from '@/modules/toast/toastStore'
-
-export const toastShow = (title, text, confirm, duration) => {
-  const toast = useToastStore()
-  return toast.createToast(title, text, confirm, duration)
-}
